795ec983383055dce7ca75cf0f63893ff6155a49
Aravind SV <arvind.sv@gmail.com>
2017-09-20 09:30:27 -0400

    Allow running git log in different dir

--- update_status.rb
+++ update_status.rb
@@ -24,17 +24,19 @@ def issue_comment_body_from_commits(org_repo, commits)
 end
 
 begin
-  %x(git log #{from_revision}~1..#{to_revision} --pretty="format:%s %H").split("\n").group_by {|line| jira_issue_from_commit_message(line)}.each do |issue_key, commits|
-    if issue_key.empty?
-      puts "Not updating Jira with #{commits.size} commit(s) since there is no issue number"
-      next
+  Dir.chdir(ARGV[0] || '.') do
+    %x(git log #{from_revision}~1..#{to_revision} --pretty="format:%s %H").split("\n").group_by {|line| jira_issue_from_commit_message(line)}.each do |issue_key, commits|
+      if issue_key.empty?
+        puts "Not updating Jira with #{commits.size} commit(s) since there is no issue number"
+        next
+      end
+
+      message = "Commits:\n#{issue_comment_body_from_commits(org_repo, commits)}"
+      command = message =~ /CLOSE/ ? 'close' : 'comment'
+
+      system(File.join(__dir__, 'jira.rb'), issue_key, command, message)
+      puts "Failed to operate on issue #{issue_key} (command: #{command}): #{$?.inspect}" unless $?.success?
     end
-
-    message = "Commits:\n#{issue_comment_body_from_commits(org_repo, commits)}"
-    command = message =~ /CLOSE/ ? 'close' : 'comment'
-
-    system(File.join(__dir__, 'jira.rb'), issue_key, command, message)
-    puts "Failed to operate on issue #{issue_key} (command: #{command}): #{$?.inspect}" unless $?.success?
   end
 rescue => e
   $stderr.puts "Failed to update status to Jira. Something went wrong: #{e.message}"
